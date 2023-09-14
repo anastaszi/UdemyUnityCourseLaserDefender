b3f624d56264e69f625d898432e4ac4308b22b10
Anastasia Zimina <anastasba@gmail.com>
2023-09-14 11:44:20 -0700

    adding boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,33 @@ public class Player : MonoBehaviour
 {
     Vector2 rawInputVector;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float paddingLeft = 1f;
+    [SerializeField] float paddingRight = 1f;
+    [SerializeField] float paddingTop = 1f;
+    [SerializeField] float paddingBottom = 1f;
+    Vector2 minScreenBounds;
+    Vector2 maxScreenBounds;
+
+    void Start () {
+        InitBounds();
+    }
     void Update()
     {
-        Vector3 delta = rawInputVector * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Move();
+    }
+
+    void InitBounds() {
+        Camera mainCamera = Camera.main;
+        minScreenBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxScreenBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
+    void Move() {
+        Vector2 delta = rawInputVector * moveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minScreenBounds.x + paddingLeft, maxScreenBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minScreenBounds.y + paddingBottom, maxScreenBounds.y - paddingTop);
+        transform.position = newPosition;
     }
 
     void OnMove(InputValue value)
